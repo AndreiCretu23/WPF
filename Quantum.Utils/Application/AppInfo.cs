b0e7b86a6d0a2974f9ff8db1b83efd4e04c9f6a9
Andrei Cretu <andrei.cretu23@gmail.com>
2018-06-24 14:09:20 +0300

    Common Paths + App Info

---
+++ Quantum.Utils/Application/AppInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Quantum.Utils
+{
+    public static class AppInfo
+    {
+        public static string ApplicationName { get { return Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName); } }
+
+        public static string ApplicationNameWithExtension { get { return AppDomain.CurrentDomain.FriendlyName; } }
+
+        public static string ApplicationDirectory { get { return AppDomain.CurrentDomain.BaseDirectory; } }
+
+        public static string ApplicationPath { get { return Path.Combine(ApplicationDirectory, ApplicationNameWithExtension); } }
+
+        public static string ApplicationRepository { get { return Path.Combine(CommonPaths.AppDataRoaming, ApplicationName); } }
+
+        public static string ApplicationConfigRepository { get { return Path.Combine(ApplicationRepository, "Config"); } }
+
+        public static int ApplicationInstanceCount { get { return Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Count(); } }
+
+        static AppInfo()
+        {
+            if(!Directory.Exists(ApplicationRepository)) {
+                Directory.CreateDirectory(ApplicationRepository);
+            }
+            if(!Directory.Exists(ApplicationConfigRepository)) {
+                Directory.CreateDirectory(ApplicationConfigRepository);
+            }
+        }
+
+    }
+}
---
+++ Quantum.Utils/IO/CommonPaths.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Quantum.Utils
+{
+    public static class CommonPaths
+    {
+        public static string Root { get { return String.Empty; } }
+        public static string Recent { get { return Environment.GetFolderPath(Environment.SpecialFolder.Recent); } }
+        public static string Desktop { get { return Environment.GetFolderPath(Environment.SpecialFolder.Desktop); } }
+        public static string Documents { get { return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); } }
+        public static string Downloads { get { return GetDownloadsFolderPath(); } }
+        public static string Music { get { return Environment.GetFolderPath(Environment.SpecialFolder.MyMusic); } }
+        public static string Pictures { get { return Environment.GetFolderPath(Environment.SpecialFolder.MyPictures); } }
+        public static string Videos { get { return Environment.GetFolderPath(Environment.SpecialFolder.MyVideos); } }
+
+        public static string AppDataLocal { get { return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData); } }
+        public static string AppDataRoaming { get { return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData); } }
+
+        #region Private
+
+        public static string GetDownloadsFolderPath()
+        {
+            string DownloadsFullPath;
+            SHGetKnownFolderPath(new Guid("374DE290-123F-4565-9164-39C4925E467B"), 0, IntPtr.Zero, out DownloadsFullPath);
+            return DownloadsFullPath;
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        private static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out string pszPath);
+
+        #endregion Private
+    }
+}
