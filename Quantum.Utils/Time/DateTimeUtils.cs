9038835bbfe8f52449958ab94371cd7cff9c411d
Andrei Cretu <andrei.cretu23@gmail.com>
2018-06-24 19:20:15 +0300

    DateTime Utilities

---
+++ Quantum.Utils/Time/DateTimeUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Utils
+{
+    public enum DateTimeReference
+    {
+        Milliseconds,
+        Seconds,
+        Minutes,
+        Hours,
+        Days,
+    }
+
+    public static class DateTimeUtils
+    {
+        public static TimeSpan GetTimeDifference(DateTime dateTime1, DateTime dateTime2)
+        {
+            TimeSpan timeSpan;
+            if (dateTime1.CompareTo(dateTime2) > 0)
+            {
+                timeSpan = dateTime1 - dateTime2;
+            }
+            else
+            {
+                timeSpan = dateTime2 - dateTime1;
+            }
+
+            return timeSpan;
+        }
+
+        public static bool IsTimeDifferenceGreaterThan(DateTime dateTime1, DateTime dateTime2, DateTimeReference dateTimeReference, double value)
+        {
+            TimeSpan timeSpan = GetTimeDifference(dateTime1, dateTime2);
+
+            switch (dateTimeReference)
+            {
+                case DateTimeReference.Milliseconds: return timeSpan.TotalMilliseconds > value;
+                case DateTimeReference.Seconds: return timeSpan.TotalSeconds > value;
+                case DateTimeReference.Minutes: return timeSpan.TotalMinutes > value;
+                case DateTimeReference.Hours: return timeSpan.TotalHours > value;
+                case DateTimeReference.Days: return timeSpan.TotalDays > value;
+                default: throw new Exception("Error : Unexpected DateTimeReference"); // This can never happen
+            }
+        }
+    }
+}
