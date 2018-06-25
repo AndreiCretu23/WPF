61f0abc4b036fb5958b057321e4e03b027a85790
Andrei Cretu <andrei.cretu23@gmail.com>
2018-06-25 19:21:52 +0300

    MissingAttributeException
    Added MissingAttributeException

--- Quantum.Core/Bootstrapper/QuantumBootstrapper.cs
+++ Quantum.Core/Bootstrapper/QuantumBootstrapper.cs
@@ -22,7 +22,7 @@ namespace Quantum.Core
 
         public void Run()
         {
-            
+
         }
     }
 }
---
+++ Quantum.Utils/Exceptions/MissingAttributeException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Quantum.Exceptions
+{
+    public class MissingAttributeException : Exception
+    {
+        public MemberInfo Member { get; private set; }
+        public Type ExpectedAttributeType { get; private set; }
+
+        public MissingAttributeException(MemberInfo memberInfo, Type attributeType)
+        {
+            Debug.Assert(typeof(Attribute).IsAssignableFrom(attributeType));
+            Member = memberInfo;
+            ExpectedAttributeType = attributeType;
+        }
+
+        public MissingAttributeException(MemberInfo memberInfo, Type attributeType, string message)
+            : base(message)
+        {
+            Debug.Assert(typeof(Attribute).IsAssignableFrom(attributeType));
+            Member = memberInfo;
+            ExpectedAttributeType = attributeType;
+        }
+    }
+}
