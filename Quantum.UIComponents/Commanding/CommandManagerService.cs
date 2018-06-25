28c93060274f0633c03551b151937337e97fa11a
Andrei Cretu <andrei.cretu23@gmail.com>
2018-06-26 00:05:09 +0300

    Basic Metadata/Command Package
    Just architecture (No implementation (yet))

---
+++ Quantum.UIComponents/Commanding/CommandManagerService.cs
@@ -0,0 +1,22 @@
+﻿using Quantum.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Command
+{
+    public interface ICommandManagerService
+    {
+
+    }
+
+    internal class CommandManagerService : QuantumServiceBase, ICommandManagerService
+    {
+        public CommandManagerService(IObjectInitializationService initSvc)
+            : base(initSvc)
+        {
+        }
+    }
+}
---
+++ Quantum.UIComponents/Commanding/MetadataDefinitions/IDefinitionMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Command
+{
+    public interface IDefinitionMetadata
+    {
+        /// <summary>
+        /// Specifies whether the metadata type supports more than one instance in a collection.
+        /// </summary>
+        bool SupportsMultiple { get; }
+    }
+}
