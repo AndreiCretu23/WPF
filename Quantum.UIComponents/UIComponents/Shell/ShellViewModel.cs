7a1fe2bf3385d55c9d04bd44f414e914a151f10c
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-07-21 17:03:12 +0300

    Shortcuts
    Shortcuts AttachedProperty + MVVM Support

--- Quantum.UIComponents/UIComponents/Shell/ShellViewModel.cs
+++ Quantum.UIComponents/UIComponents/Shell/ShellViewModel.cs
@@ -4,11 +4,30 @@ using System.Windows;
 using System.ComponentModel;
 using Microsoft.Practices.Composite.Presentation.Commands;
 using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Quantum.UIComponents
 {
     public class ShellViewModel : ViewModelBase
     {
+        [Service]
+        public ICommandManagerService CommandManager { get; set; }
+
+
+        public IEnumerable<KeyBinding> Shortcuts
+        {
+            get
+            {
+                var commands = CommandManager.ManagedCommands.Where(c => c.MainMenuMetadata.OfType<KeyShortcut>().Count() == 1);
+                foreach(var command in commands)
+                {
+                    var shortcut = command.MainMenuMetadata.OfType<KeyShortcut>().Single();
+                    yield return new KeyBinding(command, new KeyGesture(shortcut.Key, shortcut.ModifierKeys));
+                }
+            }
+        }
+        
         public IMainMenuViewModel MainMenuViewModel { get; set; }
 
         public ShellViewModel(IObjectInitializationService initSvc)
---
+++ Quantum.UIComposition/AttachedProperties/ShortcutsProperty.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Quantum.UIComposition
+{
+    public static partial class AttachedProperties
+    {
+        public static readonly DependencyProperty ShortcutsProperty =
+            DependencyProperty.RegisterAttached("Shortcuts", typeof(IEnumerable<KeyBinding>),
+                typeof(AttachedProperties), new UIPropertyMetadata(Enumerable.Empty<KeyBinding>(), ShortcutsChanged));
+
+        public static IEnumerable<KeyBinding> GetShortcuts(DependencyObject obj)
+        {
+            return (IEnumerable<KeyBinding>)obj.GetValue(ShortcutsProperty);
+        }
+
+        public static void SetShortcuts(DependencyObject obj, IEnumerable<KeyBinding> value)
+        {
+            obj.SetValue(ShortcutsProperty, value);
+        }
+
+        private static void ShortcutsChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var uiElement = obj as UIElement;
+            if(uiElement == null) {
+                throw new Exception($"Object of type '{obj.GetType().Name}' does not support InputBindings");
+            }
+
+            uiElement.InputBindings.Clear();
+
+            var keyBindings = (IEnumerable<KeyBinding>)e.NewValue;
+            foreach(var keyBinding in keyBindings)
+            {
+                uiElement.InputBindings.Add(keyBinding);
+            }
+        }
+
+    }
+}
