adf3beafed63b7d3a9779db93864b4183141135d
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-08-19 10:39:25 +0300

    DynamicPanel SelectionBinding Testing Tools

--- WPF/ToolBars/FirstToolBar/FirstToolBarViewModel.cs
+++ WPF/ToolBars/FirstToolBar/FirstToolBarViewModel.cs
@@ -1,19 +1,69 @@
-﻿using System;
+﻿using Quantum.UIComponents;
+using Quantum.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using WPF.Panels;
+using Quantum.Common;
+using Microsoft.Practices.Composite.Presentation.Commands;
 
 namespace WPF.ToolBars
 {
     [Guid("ACC63832-E337-4B27-821A-DDEEA52FD95E")]
     public interface IFirstToolBarViewModel
     {
+        DelegateCommand<object> AddPanelCommand { get; }
     }
 
     [Guid("10EE9E8C-8C29-4FAF-AFA7-B80BC63E8156")]
-    public class FirstToolBarViewModel : IFirstToolBarViewModel
+    public class FirstToolBarViewModel : ViewModelBase, IFirstToolBarViewModel
     {
+        [Selection]
+        public DynamicPanelSelection SelectedPanels { get; set; }
+
+        private IObjectInitializationService InitializationService { get; set; }
+
+        public FirstToolBarViewModel(IObjectInitializationService initSvc)
+            : base(initSvc)
+        {
+            InitializationService = initSvc;
+        }
+
+
+        public DelegateCommand<object> AddPanelCommand => new DelegateCommand<object>
+            (
+                canExecuteMethod: o => true, 
+                executeMethod: o => AddPanel()
+            );
+
+
+        private void AddPanel()
+        {
+            var panelId = GetFirstFreeNumber(SelectedPanels.Value.OfType<IIdentifiable>().Select(o => Int32.Parse(o.Guid)));
+            SelectedPanels.Value.Add(new DynamicPanelViewModel(InitializationService)
+            {
+                Guid = panelId.ToString(),
+                DisplayText = $"DynamicPanel {panelId.ToString()}"
+            });
+        }
+
+
+        private int GetFirstFreeNumber(IEnumerable<int> values)
+        {
+            int i = 1;
+            while (true)
+            {
+                if(!values.Contains(i))
+                {
+                    return i;
+                }
+
+                i++;
+            }
+        }
+
     }
 }
