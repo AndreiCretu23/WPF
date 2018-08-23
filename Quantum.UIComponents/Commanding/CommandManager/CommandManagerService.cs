6bf0d4f1643cd568dbe6a8dac3753cae53d38c41
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-08-23 23:51:45 +0300

    Minor architecture refactoring

--- Quantum.UIComponents/Commanding/CommandManager/CommandManagerService.cs
+++ Quantum.UIComponents/Commanding/CommandManager/CommandManagerService.cs
@@ -9,22 +9,6 @@ using System.Linq.Expressions;
 
 namespace Quantum.Command
 {
-    public interface ICommandManagerService
-    {
-        IEnumerable<object> Commands { get; }
-        IEnumerable<IManagedCommand> ManagedCommands { get; }
-        IEnumerable<IMultiManagedCommand> MultiManagedCommands { get; }
-        
-        void RegisterCommandContainer<IContainer, TContainer>() where TContainer : class, IContainer 
-                                                                where IContainer : class, ICommandContainer;
-
-        object GetCommand<TCommandContainer>(Expression<Func<TCommandContainer, object>> commandProperty) 
-            where TCommandContainer : ICommandContainer;
-
-        TCommand GetCommand<TCommandContainer, TCommand>(Expression<Func<TCommandContainer, TCommand>> commandProperty)
-            where TCommandContainer : ICommandContainer;
-    }
-
     internal class CommandManagerService : QuantumServiceBase, ICommandManagerService
     {
         [Service]
---
+++ Quantum.UIComponents/Commanding/CommandManager/ICommandManagerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quantum.Command
+{
+    public interface ICommandManagerService
+    {
+        IEnumerable<object> Commands { get; }
+        IEnumerable<IManagedCommand> ManagedCommands { get; }
+        IEnumerable<IMultiManagedCommand> MultiManagedCommands { get; }
+
+        void RegisterCommandContainer<IContainer, TContainer>() where TContainer : class, IContainer
+                                                                where IContainer : class, ICommandContainer;
+
+        object GetCommand<TCommandContainer>(Expression<Func<TCommandContainer, object>> commandProperty)
+            where TCommandContainer : ICommandContainer;
+
+        TCommand GetCommand<TCommandContainer, TCommand>(Expression<Func<TCommandContainer, TCommand>> commandProperty)
+            where TCommandContainer : ICommandContainer;
+    }
+}
