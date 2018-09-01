ed3422cb973d2ad5c69930a0d3e94bca9349f1ed
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-09-01 18:41:38 +0300

    Main Window Styling -> WindowShell

--- Quantum.UIComponents/UIComponents/Shell/ShellView.xaml.cs
+++ Quantum.UIComponents/UIComponents/Shell/ShellView.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Windows;
+﻿using Quantum.Controls;
 
 namespace Quantum.UIComponents
 {
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    internal partial class ShellView : Window
+    internal partial class ShellView : WindowShell
     {
         public ShellView()
         {
