a891f930c1461eb174d22058b8dd215e12d6c50a
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-09-09 15:19:07 +0300

    Removing usless "using" directives

--- Quantum.UIComponents/UIComponents/Menu/IMainMenuCommandExtractor.cs
+++ Quantum.UIComponents/UIComponents/Menu/IMainMenuCommandExtractor.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Quantum.Command;
+﻿using Quantum.Command;
 using Quantum.Metadata;
-using Quantum.Services;
+using System.Collections.Generic;
 
 namespace Quantum.UIComponents
 {
