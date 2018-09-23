85db41337b256bb0628ff0f59547200589099dec
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-09-23 19:15:08 +0300

    MultiManagedCommand summary refactoring.

--- Quantum.UIComponents/Commanding/Command/MultiManagedCommand.cs
+++ Quantum.UIComponents/Commanding/Command/MultiManagedCommand.cs
@@ -12,12 +12,12 @@ namespace Quantum.Command
     ///                          This metadata type does not support multiple instances in the metadata collection of a multi managed command. <para/>
     /// 2) AutoInvalidateOnEvent - Instructs the framework to re-evaluate the commands when the specified event type is fired in the event aggregator instance 
     ///                            of the application's container. The current set of commmands associated with this MultiManagedCommand will be wiped out, 
-    ///                            and the getter delegate will be used to re-compute the commands. After that, the commands will be re-processed by the framework 
+    ///                            and the getter delegate will be used to re-compute the commands. After that, the new commands will be re-processed by the framework 
     ///                            depending on their individual metadata. Supported event types are types which extend CompositePresentationEvent. <para/>
     /// 3) AutoInvalidateOnSelection - Instructs the framework to re-evaluate the commands when the selection of the specified type (resolved from the 
     ///                                event aggregator instance of the application's container) changes. The current set of commmands associated with 
     ///                                this MultiManagedCommand will be wiped out, and the getter delegate will be used to re-compute the commands. 
-    ///                                After that, the commands will be re-processed by the framework depending on their individual metadata.
+    ///                                After that, the new commands will be re-processed by the framework depending on their individual metadata.
     ///                                Supported selection types are types which extend SelectionBase. <para/>
     /// </summary>
     public class MultiManagedCommand : MultiStaticMetadataOwnerCommand<ISubCommand, MultiCommandMetadataCollection, IMultiCommandMetadata>, IMultiManagedCommand
