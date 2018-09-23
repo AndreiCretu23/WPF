b862c795bd784497a58d92fa1fb390af7f5a1f30
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-09-23 19:52:10 +0300

    Minor summary refactoring.

--- Quantum.UIComponents/Commanding/CommandManager/CommandAttributes.cs
+++ Quantum.UIComponents/Commanding/CommandManager/CommandAttributes.cs
@@ -3,7 +3,7 @@
 namespace Quantum.Command
 {
     /// <summary>
-    /// Signals the CommandManagerService to ignore a particular command property when registering the parent container.
+    /// Signals the CommandManagerService to ignore a particular command property when registering the parent command container.
     /// </summary>
     [AttributeUsage(validOn: AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     public class IgnoreCommandAttribute : Attribute
