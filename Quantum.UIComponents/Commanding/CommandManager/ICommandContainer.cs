fd09c045551f33081bb68a92e6e60d3dafeffe4a
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-09-23 19:52:47 +0300

    ICommandContainer summary.

--- Quantum.UIComponents/Commanding/CommandManager/ICommandContainer.cs
+++ Quantum.UIComponents/Commanding/CommandManager/ICommandContainer.cs
@@ -1,5 +1,8 @@
 ﻿namespace Quantum.Command
 {
+    /// <summary>
+    /// The basic contract for all command containers in the application.
+    /// </summary>
     public interface ICommandContainer
     {
     }
