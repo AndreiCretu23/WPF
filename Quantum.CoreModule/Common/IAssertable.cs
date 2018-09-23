c0d583bec82670cfcaaa3c5f3a3e12d433e899c9
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-09-23 08:54:21 +0300

    Common Interface Summaries

--- Quantum.CoreModule/Common/IAssertable.cs
+++ Quantum.CoreModule/Common/IAssertable.cs
@@ -1,5 +1,8 @@
 ﻿namespace Quantum.Common
 {
+    /// <summary>
+    /// This interface should be/ is implemented by types which need need to be in a certain state in order to function properly.
+    /// </summary>
     public interface IAssertable
     {
         void Assert(string objName = null);
--- Quantum.CoreModule/Common/IIdentifiable.cs
+++ Quantum.CoreModule/Common/IIdentifiable.cs
@@ -1,5 +1,10 @@
-﻿namespace Quantum.Common
+﻿using System;
+
+namespace Quantum.Common
 {
+    /// <summary>
+    /// Defines the object as unique, idenfiable by a custom id.
+    /// </summary>
     public interface IIdentifiable
     {
         string Guid { get; }
