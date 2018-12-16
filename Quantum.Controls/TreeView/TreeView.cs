2444ea665d673087c7ed67364fe521f07e65411c
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-12-16 19:33:13 +0200

    TreeView 1
    Basic source structure (control sources + default styles).

---
+++ Quantum.Controls/TreeView/TreeView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Quantum.Controls
+{
+    public class TreeView : System.Windows.Controls.ItemsControl
+    {
+        static TreeView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeView), new FrameworkPropertyMetadata(typeof(TreeView)));
+        }
+    }
+}
---
+++ Quantum.Controls/TreeViewItem/TreeViewItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Quantum.Controls
+{
+    public class TreeViewItem : System.Windows.Controls.ItemsControl
+    {
+        static TreeViewItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeViewItem), new FrameworkPropertyMetadata(typeof(TreeViewItem)));
+        }
+    }
+}
