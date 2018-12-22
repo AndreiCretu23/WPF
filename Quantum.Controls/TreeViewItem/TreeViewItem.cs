4c0cdbe1ed01a116ddf888abaf5db761da607961
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-12-22 17:16:38 +0200

    TreeView(Item) -> Styling part 2

--- Quantum.Controls/TreeViewItem/TreeViewItem.cs
+++ Quantum.Controls/TreeViewItem/TreeViewItem.cs
@@ -22,8 +22,41 @@ namespace Quantum.Controls
             typeMetadata: new PropertyMetadata(defaultValue: false)
         );
 
+        public static readonly DependencyProperty IsCheckableProperty = DependencyProperty.Register
+        (
+            name: "IsCheckable",
+            propertyType: typeof(bool),
+            ownerType: typeof(TreeViewItem),
+            typeMetadata: new PropertyMetadata(defaultValue: false)
+        );
+
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register
+        (
+            name: "IsChecked",
+            propertyType: typeof(bool),
+            ownerType: typeof(TreeViewItem),
+            typeMetadata: new PropertyMetadata(defaultValue: false)
+        );
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register
+        (
+            name: "Icon",
+            propertyType: typeof(ImageSource), 
+            ownerType: typeof(TreeViewItem),
+            typeMetadata: new PropertyMetadata(null)
+        );
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register
+        (
+            name: "Header",
+            propertyType: typeof(string),
+            ownerType: typeof(TreeViewItem),
+            typeMetadata: new PropertyMetadata(null)
+        );
+
         #endregion DependencyProperties
 
+        
         #region Properties
 
         public bool IsExpanded
@@ -32,6 +65,31 @@ namespace Quantum.Controls
             set { SetValue(IsExpandedProperty, value); }
         }
 
+        public bool IsCheckable
+        {
+            get { return (bool)GetValue(IsCheckableProperty); }
+            set { SetValue(IsCheckableProperty, value); }
+        }
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+        
         #endregion Properties
 
 
