3c0a2b4711a580352748e8c2e898ebb721d83c55
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-12-29 19:34:03 +0200

    Summaries

--- Quantum.UIComponents/ViewModel/Contract/IViewModel.cs
+++ Quantum.UIComponents/ViewModel/Contract/IViewModel.cs
@@ -3,6 +3,9 @@ using Quantum.UIComposition;
 
 namespace Quantum.UIComponents
 {
+    /// <summary>
+    /// Defines the basic contract for a ViewModel that has access to the container, services and events.
+    /// </summary>
     public interface IViewModel : IObservableObject, IDestructible
     {
     }
