01f2ac4d98c8013985468ec78979332179cf25a7
AndreiCretu23 <40534705+AndreiCretu23@users.noreply.github.com>
2018-12-31 19:07:47 +0200

    c# 7.2 + Basic ViewModel contract infrastructure

---
+++ Quantum.UIComponents/ViewModel/Contract/IViewModelItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.UIComponents
+{
+    public interface IViewModelItem : IViewModel
+    {
+        object Value { get; }
+        IRootViewModel Root { get; }
+        bool IsSelected { get; }
+    }
+}
---
+++ Quantum.Utils/Misc/ManualObservableCollection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Utils
+{
+    /// <summary>
+    /// Represents an observable collection in which the collection changed event is triggered manually.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ManualObservableCollection<T> : ObservableCollection<T>
+    {
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            collection.AssertParameterNotNull(nameof(collection));
+            foreach(var element in collection) {
+                Add(element);
+            }
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            // Do nothing
+        }
+
+        public void RaiseCollectionChanged()
+        {
+            base.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+
+}
